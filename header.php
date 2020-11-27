22dfecd28972a80196f4f1af5002973d8b13d29e
rijushm <64298473+rijushm@users.noreply.github.com>
2020-11-27 19:36:21 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<nav>
+        <div class="logo">
+            <img src="img/logo.png" alt="Logo Image">
+        </div>
+        <div class="hamburger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Free Study Material</a></li>
+            <li><a href="#">Fee Stuctures</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><button class="join-button" href="registration.php">Join US</button></li>
+        </ul>
+    </nav>
